8a9c3600c2c080c202103d320c2ac0f8b5b57f44
SayanH-BIT <sayan.haldar.brainium@gmail.com>
2026-02-23 13:41:25 +0530

    main jsx nd vite.config updated

--- zustcart/src/App.jsx
+++ zustcart/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import './App.css';
+import Footer from './components/Footer';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [productsData, setProductsData] = useState([]);
+
+  async function fetchProducts() {
+    try {
+      const products = await axios.get('http://localhost:8080/api/products');
+      setProductsData(products.data.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100">
+      <Navbar onCartClick={() => setIsCartOpen(true)} productsData={productsData} />
+      <Products productsData={productsData} loading={loading} />
+      <Cart isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} productsData={productsData} />
+      <Footer />
+    </div>
+  );
 }
-
-export default App
--- zustcart/vite.config.js
+++ zustcart/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      react: resolve(__dirname, 'node_modules/react'),
+      'react-dom': resolve(__dirname, 'node_modules/react-dom')
+    }
+  }
 })
