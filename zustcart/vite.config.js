21306035c5126020335cc041feb6f7b8c35d4668
SayanH-BIT <sayan.haldar.brainium@gmail.com>
2026-02-23 13:53:28 +0530

    vite.config and package file updated for deployment

--- zustcart/vite.config.js
+++ zustcart/vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ZustCart/",
   resolve: {
     alias: {
       react: resolve(__dirname, 'node_modules/react'),
